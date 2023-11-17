4416eaecbe300e41203d3f25f7a7c9b6adf63477
Bruxar <larancibia@utem.cl>
2023-11-17 16:53:49 -0300

    Refactor responsive layout and fix menu behavior

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 document.body.addEventListener('click', function(event) {
     var menu = document.getElementById("menu");
-    var header = document.querySelector("header"); // Cambio aquí
+    var header = document.querySelector("header");
 
-    // Si el clic no fue en el menú ni en el botón del menú, cierra el menú
-    if (!event.target.closest('#menu') && !event.target.classList.contains('menu-btn')) {
+    // Obtener el ancho actual de la ventana
+    var windowWidth = window.innerWidth;
+
+    // Verificar si el clic no fue en el menú ni en el botón del menú y la pantalla es pequeña
+    if (!event.target.closest('#menu') && !event.target.classList.contains('menu-btn') && windowWidth <= 600) {
         menu.classList.remove('show');
-        document.body.classList.remove('menu-open'); // Agregado para controlar el estilo del body
-        header.classList.remove('column'); // Agregado para quitar la clase 'column' cuando se cierra el menú
+        document.body.classList.remove('menu-open');
+        header.classList.remove('column');
     }
 });
 
 function toggleMenu() {
-    var menu = document.getElementById("menu");
-    var body = document.body;
-    var header = document.querySelector("header"); // Cambio aquí
+    // Obtener el ancho actual de la ventana
+    var windowWidth = window.innerWidth;
 
-    menu.classList.toggle("show");
-    body.classList.toggle("menu-open"); // Agregado para controlar el estilo del body
-    header.classList.toggle("column"); // Cambio aquí
+    // Verificar si la pantalla es pequeña antes de ejecutar toggleMenu
+    if (windowWidth <= 600) {
+        var menu = document.getElementById("menu");
+        var body = document.body;
+        var header = document.querySelector("header");
+
+        menu.classList.toggle("show");
+        body.classList.toggle("menu-open");
+        header.classList.toggle("column");
+    }
 }
 
 // Agregar eventos de clic a los enlaces y al botón "Contáctanos"
-var links = document.querySelectorAll('.header-secciones a, .contacto-btn'); // Agregado aquí
-links.forEach(function(link) { // Agregado aquí
-    link.addEventListener('click', function() { // Agregado aquí
-        toggleMenu(); // Agregado aquí
-    }); // Agregado aquí
-}); // Agregado aquí
+var links = document.querySelectorAll('.header-secciones a, .contacto-btn');
+links.forEach(function(link) {
+    link.addEventListener('click', function() {
+        toggleMenu();
+    });
+});
